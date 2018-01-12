93a71f151a93ccd42e1f594355068b8dcdb1022f
Derik Linch <dlinch33@gmail.com>
2018-01-12 14:51:30 -0700

    initial commit

---
+++ src/models/pet.cr
@@ -0,0 +1,13 @@
+require "granite_orm/adapter/pg"
+
+class Pet < Granite::ORM::Base
+  adapter pg
+  table_name pets
+
+
+  # id : Int64 primary key is created for you
+  field name : String
+  field breed : String
+  field age : Int32
+  timestamps
+end
